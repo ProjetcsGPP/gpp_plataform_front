7ff7fd594914ad062d81ac99fa5f0bdb5869e441
ProjetcsGPP <gpp@seger.es.gov.br>
2026-04-17 15:20:40 -0300

    fix(portal): corrige export default em aplicacoes/page.tsx

--- src/app/portal/aplicacoes/page.tsx
+++ src/app/portal/aplicacoes/page.tsx
@@ -1,4 +1,11 @@
 // src/app/portal/aplicacoes/page.tsx
-import { UnderDevelopment } from "../../../components/common/UnderDevelopment";
+import { UnderDevelopment } from '@/components/common/UnderDevelopment'
 
-<UnderDevelopment />;
+export default function AplicacoesPage() {
+  return (
+    <UnderDevelopment
+      featureName="Aplicações"
+      forecast="em breve"
+    />
+  )
+}
